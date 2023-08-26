bed3d3fb6e9f930aa5f8e350dfe367906836de06
Vishu <Vishwajeetsinghchauhan63@gmail.com>
2023-08-26 19:21:04 +0530

    Route setup done

--- youtube-clone/src/App.jsx
+++ youtube-clone/src/App.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
 
-export default function App()
-{
-    return (
-        <div>
-            <h1>Hello</h1>
-        </div>
-    )
-}
+import Navbar from './components/Navbar';
+
+
+
+const App = () => (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: '#000' }}>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Feed />} />
+          <Route path='/video/:id' element={<VideoDetail />} />
+          <Route path='/channel/:id' element={<ChannelDetail />} />
+          <Route path='/search/:searchTerm' element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
+  
+  export default App;
