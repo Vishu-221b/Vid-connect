e674b51d70221bb064bd6ae2f0f9a38e511872b1
Vishu <Vishwajeetsinghchauhan63@gmail.com>
2023-08-30 15:55:04 +0530

    navbar done

--- youtube-clone/src/App.jsx
+++ youtube-clone/src/App.jsx
@@ -4,13 +4,30 @@ import { Box } from '@mui/material';
 import Navbar from './components/Navbar';
 
 
+const App = () => (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: '#000' }}>
+        <Navbar />
+      </Box>
+    </BrowserRouter>
+  );
+  
+  export default App;
+
+
+ /*  import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import {Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from './components';
+
+
 
 const App = () => (
     <BrowserRouter>
       <Box sx={{ backgroundColor: '#000' }}>
         <Navbar />
         <Routes>
-          <Route exact path='/' element={<Feed />} />
+          <Route path='/' element={<Feed />} />
           <Route path='/video/:id' element={<VideoDetail />} />
           <Route path='/channel/:id' element={<ChannelDetail />} />
           <Route path='/search/:searchTerm' element={<SearchFeed />} />
@@ -19,4 +36,4 @@ const App = () => (
     </BrowserRouter>
   );
   
-  export default App;
+  export default App; */
